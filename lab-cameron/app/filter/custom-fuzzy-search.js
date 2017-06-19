51f554c2f8fb6f008fb0e21585918cb528841fb7
Cameron Bacon <cameronmbacon@gmail.com>
2017-06-19 15:11:19 -0700

    merging before team fork

--- lab-cameron/app/filter/custom-fuzzy-search.js
+++ lab-cameron/app/filter/custom-fuzzy-search.js
@@ -1,13 +1,13 @@
 'use strict';
 
 module.exports = function() {
-  return function(galleries, string='') {
-    let matched = [];
-    galleries.forEach(gallery => {
-      let beginsWith = gallery.name.toLowerCase().substring(0, string.length);
-      if (beginsWith === string.toLowerCase()) matched.push(gallery);
-    });
-    console.log(matched);
-    return matched;
+  return function(galleries, searchTerm) {
+    let fuzzyPajamas = generateFuzzyPajamas(searchTerm);
+    return galleries.filter(gallery => fuzzyPajamas.test(gallery.name.toLowerCase()));
   };
+
+  function generateFuzzyPajamas(input) {
+    if (!input) return /.*/;
+    return new RegExp(`.*${input.toLowerCase().split('').join('.*')}.*`);
+  }
 };
